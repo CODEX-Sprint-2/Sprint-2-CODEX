59195435765372ab0c87749d4151d728bde2e214
isaac Ambrozevicius <ipambrozevicius@gmail.com>
2026-05-15 15:17:02 -0300

    continuação do login

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pinInserido = "";
-const pinCorreto = "0000";
+const pinCorreto = "1234";
 
 const dots = document.querySelectorAll('.ponto');
 
@@ -12,3 +12,31 @@ function atualizarDots() {
         }
     });
 }
+
+function validarPin() {
+    if (pinInserido === pinCorreto) {
+        window.location.href = 'tela_inicial.html';
+    } else {
+        alert('PIN incorreto! Tente 1234');
+        pinInserido = '';
+        atualizarDots();
+    }
+}
+
+function addNumber(num) {
+    if (pinInserido.length < 4) {
+        pinInserido += num;
+        atualizarDots();
+    }
+
+    if (pinInserido.length === 4) {
+        setTimeout(validarPin, 200);
+    }
+}
+
+function deleteNumber() {
+    pinInserido = pinInserido.slice(0, -1);
+}
+
+window.addNumber = addNumber;
+window.deleteNumber = deleteNumber;
